a2776cf6e95cf7240d16ff709050d7b0c90415e4
Guy Kaganovsky <guykaganovsky@Guys-MacBook-Pro.local>
2026-03-09 22:04:28 +0700

    Deploy: Update config for port 3334

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [{
     name: "ask-gamblers",
-    script: "./server.js",
-    instances: "max",
-    exec_mode: "cluster",
+    script: "npx",
+    args: "next start -p 3334 -H 0.0.0.0",
+    instances: 1,
+    exec_mode: "fork",
     autorestart: true,
     watch: false,
     max_memory_restart: "1G",
+    cwd: "/home/master/applications/dzdatjcdrp/public_html",
     env_production: {
-      NODE_ENV: "production",
-      PORT: 3000,
-      HOSTNAME: "0.0.0.0"
+      NODE_ENV: "production"
     }
   }]
 };
