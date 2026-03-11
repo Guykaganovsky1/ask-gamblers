70ef35a5a695d4ef33f9323ff7a9ce506f46f397
Deploy <deploy@cloudways.com>
2026-03-11 19:42:04 +0700

    Update WhatsApp number

--- components/ui/whatsapp-button.tsx
+++ components/ui/whatsapp-button.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const WHATSAPP_NUMBER = "972509200920";
+const WHATSAPP_NUMBER = "0509200920";
 const WHATSAPP_MESSAGE = "שלום, הגעתם לעסק גמבלרס. השאירו הודעה ונחזור אליכם בהקדם. תודה";
 
 export function WhatsAppButton() {
